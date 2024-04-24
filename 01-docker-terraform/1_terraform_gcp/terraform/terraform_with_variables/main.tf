c95e62924c9e6b7f94b79cee853edb65d61e8f53
Gregory Clunies <greg.clunies@gmail.com>
2024-04-24 14:22:41 -0400

    terraform lesson notes

--- 01-docker-terraform/1_terraform_gcp/terraform/terraform_with_variables/main.tf
+++ 01-docker-terraform/1_terraform_gcp/terraform/terraform_with_variables/main.tf
@@ -13,7 +13,7 @@ provider "google" {
   region      = var.region
 }
 
-resource "google_storage_bucket" "demo-bucket" {
+resource "google_storage_bucket" "gclunies-demo-bucket" {
   name          = var.gcs_bucket_name
   location      = var.location
   force_destroy = true
@@ -27,7 +27,7 @@ resource "google_storage_bucket" "demo-bucket" {
   }
 }
 
-resource "google_bigquery_dataset" "demo_dataset" {
+resource "google_bigquery_dataset" "my_demo_dataset" {
   dataset_id = var.bq_dataset_name
   location   = var.location
 }
--- 01-docker-terraform/1_terraform_gcp/terraform/terraform_with_variables/variables.tf
+++ 01-docker-terraform/1_terraform_gcp/terraform/terraform_with_variables/variables.tf
@@ -1,6 +1,6 @@
 variable "credentials" {
-  description = "My Credentials"
-  default     = "~/.google/credentials/google_credentials.json"
+  description = "Credentials for terraform to autenticate with GCP"
+  default     = "~/.google/credentials/terraform_runner_gcp_creds.json"
   #ex: if you have a directory where this file is called keys with your service account json file
   #saved there as my-creds.json you could use default = "./keys/my-creds.json"
 }
@@ -25,13 +25,13 @@ variable "location" {
 variable "bq_dataset_name" {
   description = "My BigQuery Dataset Name"
   #Update the below to what you want your dataset to be called
-  default     = "demo_dataset"
+  default     = "terraform_demo_dataset"
 }
 
 variable "gcs_bucket_name" {
   description = "My Storage Bucket Name"
   #Update the below to a unique bucket name
-  default     = "terraform-demo-terra-bucket"
+  default     = "terraform-demo-bucket-gclunies"
 }
 
 variable "gcs_storage_class" {
